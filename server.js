2d157f67003f6ec7beae27bd636160140f5ab3bf
zliu <60162477+NickyL279@users.noreply.github.com>
2021-12-14 22:27:03 -0500

    v1

---
+++ server.js
@@ -0,0 +1,42 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+    res.send('Hello. This is our final project back end.')
+})
+
+const session = require('express-session')
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+}))
+
+const mongoose = require('mongoose');
+mongoose.connect(
+    'mongodb://localhost:27017/webdev',
+    {useNewUrlParser: true, useUnifiedTopology: true});
+
+
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', "http://localhost:3000");
+    res.header('Access-Control-Allow-Headers',
+        'Content-Type, X-Requested-With, Origin');
+    res.header('Access-Control-Allow-Methods',
+        'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+    res.header("Access-Control-Allow-Credentials", "true");
+    next();
+});
+
+require('./src/reducers/users-reducer')(app)
+require('./src/reducers/bookmark-reducer')(app)
+require('./src/reducers/recommendation-reducer')(app)
+require('./src/reducers/producerFilms-reducer')(app)
+
+//app.listen(process.env.PORT || 4000)
+app.listen(4000)
